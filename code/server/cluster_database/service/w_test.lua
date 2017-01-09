2429b05eb8ec28676ebd939e34fbdc2ea5d16372
interfacekun <627795061@qq.com>
2017-01-09 16:29:16 +0800

    change cjson

--- code/server/cluster_database/service/w_test.lua
+++ code/server/cluster_database/service/w_test.lua
@@ -1,8 +1,15 @@
 local skynet = require "skynet"
 require "sgoly_printf"
+-- package.path = "/usr/local/share/lua/5.3/?.lua;/usr/local/share/lua/5.3/?/init.lua;/usr/local/lib/lua/5.3/?.lua;/usr/local/lib/lua/5.3/?/init.lua;./?.lua;./?/init.lua;" .. package.path
+-- package.cpath = "" .. package.cpath
+--local cjson = require "cjson"
 
 skynet.start(function()
+	printI(package.cpath)
+	-- local lua_value = {true, {foo="bar"}} 
+	-- local json_text = cjson.encode(lua_value)
 
-	printI("test")
+	-- printI(json_text)
+	skynet.exit()
 	
 end)
