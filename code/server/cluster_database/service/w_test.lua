543f0b8285809781cb8f99c772aa2352ebe4c6a7
lvyuchao <767796051@qq.com>
2017-01-09 16:58:26 +0800

    w_test

--- code/server/cluster_database/service/w_test.lua
+++ code/server/cluster_database/service/w_test.lua
@@ -1,15 +1,14 @@
 local skynet = require "skynet"
 require "sgoly_printf"
--- package.path = "/usr/local/share/lua/5.3/?.lua;/usr/local/share/lua/5.3/?/init.lua;/usr/local/lib/lua/5.3/?.lua;/usr/local/lib/lua/5.3/?/init.lua;./?.lua;./?/init.lua;" .. package.path
--- package.cpath = "" .. package.cpath
---local cjson = require "cjson"
+package.cpath = "../luaclib/lib/lua/5.3/?.so;" .. package.cpath
+local cjson = require "cjson"
 
 skynet.start(function()
 	printI(package.cpath)
-	-- local lua_value = {true, {foo="bar"}} 
-	-- local json_text = cjson.encode(lua_value)
+	local lua_value = {true, {foo="bar"}} 
+	local json_text = cjson.encode(lua_value)
 
-	-- printI(json_text)
+	printI(json_text)
 	skynet.exit()
 	
 end)
