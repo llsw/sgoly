4e0a2d9b4c3a829a055de4d41961310f499cb515
interfacekun <627795061@qq.com>
2017-01-12 11:04:17 +0800

    change get save uuid 2017年 01月 12日 星期四 11:04:05 CST

--- code/server/lualib/sgoly_tool.lua
+++ code/server/lualib/sgoly_tool.lua
@@ -2,19 +2,23 @@ require "sgoly_query"
 local sgoly_tool = {}
 
 local function saveUuid(uuid)
-	redis_query({"set","uuid",tostring(uuid)})
+	redis_query({"set","uuid", uuid})
 end
 
 local function getUuid()
 
 	local uuid = redis_query({"get", "uuid"})
-	return uuid
+	return tonumber(uuid)
 end
 
 
 
 function sgoly_tool.getUuid()
-	return lock(getUuid)
+	return getUuid()
+end
+
+function sgoly_tool.saveUuid(uuid)
+	saveUuid(uuid)
 end
 
 return sgoly_tool
