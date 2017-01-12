c7a2e42592560b99fb8d00cb0c9c1932908d608d
GitHubNull <641570479@qq.com>
2017-01-12 17:42:15 +0800

    完成签到数据操作模块的初步功能

--- code/server/lualib/sgoly_signin_record.lua
+++ code/server/lualib/sgoly_signin_record.lua
@@ -14,9 +14,9 @@ local dtvalid = require "sgoly_dateformat_valid"
 
 --[[
 函数说明：
-		函数作用：
-		传入参数：
-		返回参数：
+		函数作用：指定的昵称和日期的签到数据是否存在
+		传入参数：nickname(昵称), dt(日期 形如:2017-01-10)
+		返回参数：存在则返回true 反之返回false
 --]]
 function exist_signin_record(nickname, dt)
 	printD("exist_signin_record(%s, %s)", nickname, dt)
@@ -32,7 +32,7 @@ end
 
 --[[
 函数说明：
-		函数作用：
+		函数作用：签到参数检查
 		传入参数：nickname(昵称), dt(日期)
 		返回参数：false和msg(错误信息)或true和uid(昵称对应的用户的id)
 --]]
@@ -86,5 +86,41 @@ end
  		end
  	end
  end
+
+--[[
+函数说明：
+		函数作用：获取日期对应的签到值,用于判定给定日期是否签到
+		传入参数：日期(格式形如:2017-01-10)的一个table
+		返回参数：true和返回日期对应签到与否的值的表 或者 false和msg错误信息
+--]]
+function signin_record.get_signin_days(nickname,dts)
+	if((nil == nickname) or ("" == nickname)) then
+		return false, "昵称空值错误"
+	else
+		local uid = users.get_uid(nickname)
+		if(nil == uid) then
+			return false, "不存在该用户"
+		else
+			for k, dt in pairs(dts) do
+				local res, msg = dtvalid.valid(dt)
+				if(false == res) do
+					return false, msg
+				end
+			end
+			local res_table = {}
+			for k, dt in pairs(dts) do
+				local sql = string.format("select count from sgoly.signin_record"
+				.." where uid = '%d' and  signin_date = '%s' ;", uid, dt)
+				local tmptable = mysql_query(sql)
+				if(1 == #tmptable) then
+					res_table[dt] = 1
+				else
+					res_table[dt] = 0
+				end
+			end
+			return true, res_table
+		end
+	end
+end
 
  return signin_record
