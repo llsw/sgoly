d120b315925a45199b78945ec53c698fe6893f15
interfacekun <627795061@qq.com>
2017-01-13 17:41:56 +0800

    添加概要设计文档

--- code/server/cluster_database/service/w_test.lua
+++ code/server/cluster_database/service/w_test.lua
@@ -16,9 +16,13 @@ skynet.start(function()
 
 	printI(cjson.encode(x))
 
-	redis_query({"hset", "user:1234", "test", 5000000})
-	skynet.error(redis_query({"hget", "user:1234", "test"}))
-
+	-- redis_query({"hset", "user:1234", "test", 5000000})
+	-- skynet.error(redis_query({"hget", "user:1234", "test"}))
+	local t = redis_query({"keys", "user:*"})
+	for k,v in pairs(t) do
+		skynet.error(k,v)
+		skynet.error(k,string.match(v,"user:(.+)"))
+	end
 	
 	skynet.exit()
 	
