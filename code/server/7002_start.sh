b0e1c1cd1a68d8ccfab4cdf8ff92ae7dfaf0bac1
interfacekun <627795061@qq.com>
2017-02-09 17:42:24 +0800

    add start.sh 2017年 02月 09日 星期四 17:42:22 CST

---
+++ code/server/7002_start.sh
@@ -0,0 +1,13 @@
+
+gnome-terminal --geometry 70x33+47--10 -x bash -c "cd skynet && ./skynet ../cluster_database/config/config" &
+sleep 5
+gnome-terminal --geometry 70x33+47--10 -x bash -c "cd skynet && ./skynet ../cluster_login/config/config" &
+sleep 1
+gnome-terminal --geometry 70x33+47--10 -x bash -c "cd skynet && ./skynet ../cluster_game/config/config" &
+sleep 1
+gnome-terminal --geometry 70x33+47--10 -x bash -c "cd skynet && ./skynet ../cluster_gateway/config/config" &
+sleep 1
+gnome-terminal --geometry 70x33+47--10 -x bash -c "cd skynet && ./skynet ../cluster_rank/config/config" &
+sleep 1
+gnome-terminal --geometry 70x33+47--10 -x bash -c "/home/interface/ngrok/iosuzy_ngrok/linux/ngrok -config=/home/interface/ngrok/iosuzy_ngrok/linux/ngrok.cfg start sgoly3" &
+sleep 2
