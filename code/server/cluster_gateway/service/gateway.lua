d58a951c7c2d15b3d0281fafc5728bd54d62e81a
interfacekun <627795061@qq.com>
2017-02-15 11:20:36 +0800

    add line 2017年 02月 15日 星期三 11:20:34 CST

--- code/server/cluster_gateway/service/gateway.lua
+++ code/server/cluster_gateway/service/gateway.lua
@@ -16,37 +16,39 @@ local connection = {}
 local handler = {}
 
 function handler.open(source, conf)
-	printI("Gateway open source[%d]", source)
+	printD("Gateway open source[%d]", source)
 end
 
 function handler.message(fd, msg)
 	if msg then
-		printI("client %s says:",fd)
+		printD("client %s says:",fd)
 		local mes=sgoly_pack.decode(msg)
 		if mes.ID~="1" and mes.ID~="2"  then
 			mes.NAME=tonumber(mes.NAME)
 		end
 		local getMsgTime = os.clock()
-		printI("Get a Msg session[%s] cluster[%s] service[%s cmd[%s] id[%s] name[%s] time[%s] [%s]",
+		printD("======================MSG start============================")
+		printD("Get a Msg session[%s] cluster[%s] service[%s cmd[%s] id[%s] name[%s] time[%s] [%s]",
 			mes.SESSION, mes.CLUSTER, mes.SERVICE, mes.CMD,mes.ID, mes.NAME, getMsgTime, os.date("%H:%M:%S", os.time()))
 		local cnode=tonumber(mes.CLUSTER)
 		local snode=tonumber(mes.SERVICE)
 		cluster.call("cluster_game",".agent","setline",fd)
 		local req=cluster.call(code[cnode],code[snode],mes.CMD,fd,mes)
-		printI("End handle a Msg session[%s]  name[%s]  handlerCostTime[%s]",
+		printD("End handle a Msg session[%s]  name[%s]  handlerCostTime[%s]",
 			mes.SESSION, mes.NAME, os.clock() - getMsgTime)
 		if req~=nil then 
 		  driver.send(fd,req)
-		  printI("End send a Msg session[%s]  name[%s]  totleCostTime[%s] [%s]",
+		  printD("End send a Msg session[%s]  name[%s]  totleCostTime[%s] [%s]",
 			mes.SESSION, mes.NAME, os.clock() - getMsgTime, os.date("%H:%M:%S", os.time()))
         end
+        printD("----------------------MSG end------------------------------")
     end
 end
 
 
 function handler.connect(fd,addr)
 	gateserver.openclient(fd)
-	printI("Client fd[%d] connect gateway", fd)
+	printD("Client fd[%d] connect gateway", fd)
 	local session=1
 	local ses=tostring("fd-"..fd..":session*"..session)
 	local rep={SESSION=ses,ID="0"}
@@ -57,10 +59,10 @@ end
 
 function handler.disconnect(fd)
 	local req1=cluster.call("cluster_game",".agent","errorexit",fd)
-    printI("save 自动模式".." " ..req1)
+    printD("save 自动模式".." " ..req1)
 	local req=cluster.call("cluster_game",".agent","close",fd)
-	printI("save 普通模式".." " ..req)
-	printI("Client fd[%d] disconnect gateway", fd)
+	printD("save 普通模式".." " ..req)
+	printD("Client fd[%d] disconnect gateway", fd)
 	connection[fd] = nil
 end
 
@@ -103,20 +105,22 @@ function handlerfork(fd,name,session)
 	while true do
 		skynet.sleep(2000)
 		local line =  cluster.call("cluster_game",".agent","getline",fd)
-		printI("name[%s] fd[%d] sleep end", name, fd)
+		printD("name[%s] fd[%d] sleep end", name, fd)
 		if  line==false then
-			printI("line=false")
+			printD("line=false")
 			break
 		end
 		local timeBetween = os.time() - line
 
 		if(timeBetween > 20) then
-			printI("HeartBeat name[%s] fd[%d] timeBetween[%d]", name, fd, timeBetween)
+			printD("======================HeartBeat start============================")
+			printD("HeartBeat name[%s] fd[%d] timeBetween[%d]", name, fd, timeBetween)
 			local req={ID="13",TYPE="heart"}
 			local req2_1=sgoly_pack.encode(req)
 		    driver.send(fd,req2_1)
-		    printI("End send a heartbeat  name[%s]  time[%s]",
+		    printD("End send a heartbeat  name[%s]  time[%s]",
 			 name, os.clock())
+		    printD("----------------------HeartBeat end------------------------------")
     	end
 
     	if(timeBetween > 50) then
@@ -124,7 +128,7 @@ function handlerfork(fd,name,session)
     	end
     end
     gateserver.closeclient(fd)
-    printI(">50s fd[%d]",fd)
+    printD(">50s fd[%d]",fd)
 end
 function handler.command(cmd, source, ...)
 	local f = assert(CMD[cmd])
