795bbcaf4448eb8c7506d5b895f00e0c85b017b7
lvyuchao <767796051@qq.com>
2017-02-21 17:03:36 +0800

    lyc2017年 02月 21日 星期二 17:03:36 CST

--- code/server/cluster_game/service/maingame.lua
+++ code/server/cluster_game/service/maingame.lua
@@ -522,7 +522,7 @@ for i=1,k do
 		printD("3 money[%s]/depost[%s]=%s 进入简单模式",money,deposit,money/deposit)
 		money=0 
 		deposit=0
-	elseif historynum%10==0 and money/deposit>0.9 and money/deposit<0.75 then
+	elseif historynum%10==0 and money/deposit<0.9 and money/deposit>0.75 then
 		x=1
 		sgoly_tool.saveStatementsToRedis(name,0,0,0,0,0,0,0,x,os.date("%Y-%m-%d")) 
 		printD("2 money[%s]/depost[%s]=%s 进入普通模式", money,deposit,money/deposit)
