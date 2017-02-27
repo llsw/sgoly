4335ad56bcf516028c969744db3a7ee476d694a6
lvyuchao <767796051@qq.com>
2017-02-27 18:04:11 +0800

    lyc 2017年 02月 27日 星期一 18:04:08 CST

--- code/server/lualib/test.lua
+++
@@ -1,6 +0,0 @@
-package.cpath = "../luaclib/reload/reload.so;"-- .. package.cpath
-require "reload"
-print(package.path)
-print("hello world")
-print("hihi")
--- cpath = root.."cservice/?.so;" .. root .. "../luaclib/reload/reload.so;"
