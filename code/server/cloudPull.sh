64d44ea7c8b517be106ae2c2d91689aa707140fa
interfacekun <627795061@qq.com>
2017-03-08 11:40:09 +0800

    change sh 2017年 03月 08日 星期三 11:40:08 CST

--- code/server/cloudPull.sh
+++ code/server/cloudPull.sh
@@ -2,3 +2,6 @@
 cp ./lualib/sgoly_service_config.bak ./lualib/sgoly_service_config.lua
 git pull origin dev
 cp ./lualib/sgoly_service_config.bak2 ./lualib/sgoly_service_config.lua
+echo -e "serverclose\n" | nc 127.0.0.1 18002
+./killServer.sh
+./7002_start.sh
