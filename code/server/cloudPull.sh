450fe9bd62215867e4ac46de2d2ff4bd60c7c4b3
interfacekun <627795061@qq.com>
2017-03-15 09:48:50 +0800

    add save to redis 2017年 03月 15日 星期三 09:48:47 CST

--- code/server/cloudPull.sh
+++ code/server/cloudPull.sh
@@ -3,5 +3,6 @@ cp ./lualib/sgoly_service_config.bak ./lualib/sgoly_service_config.lua
 git pull origin dev
 cp ./lualib/sgoly_service_config.bak2 ./lualib/sgoly_service_config.lua
 echo -e "serverclose\n" | nc 127.0.0.1 18002
+echo -e "auth lkgame123\nsave\n" | redis-cli
 ./killServer.sh
 ./7002_start.sh
