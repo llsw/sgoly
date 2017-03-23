4a73b96ce84186e3c83913cba1ca63441c68fd15
interfacekun <627795061@qq.com>
2017-03-23 16:47:43 +0800

    add sleep 2017年 03月 23日 星期四 16:47:43 CST

--- code/server/cloudPull.sh
+++ code/server/cloudPull.sh
@@ -3,6 +3,7 @@ cp ./lualib/sgoly_service_config.bak ./lualib/sgoly_service_config.lua
 git pull origin dev
 cp ./lualib/sgoly_service_config.bak2 ./lualib/sgoly_service_config.lua
 echo -e "serverclose\n" | nc 127.0.0.1 18002
+sleep 5
 echo -e "auth lkgame123\nsave\n" | redis-cli
 ./killServer.sh
 ./7002_start.sh
